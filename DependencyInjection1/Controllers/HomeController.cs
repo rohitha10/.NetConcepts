f8ed400c0683a518d3ae0fead79bcd5c12ee8501
rohitha10 <chennarohitha1098@gmail.com>
2020-05-22 13:22:06 +0530

    first commit

---
+++ DependencyInjection1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public List<Employee> Index()
+        {
+            return _employeeRepository.GetEmployees();
+        }
+    }
+}
---
+++ DependencyInjection1/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection1.Models
+{
+    public class MockEmployeeRepository:IEmployeeRepository
+    {
+        private List<Employee> employees;
+        public MockEmployeeRepository()
+        {
+            employees = new List<Employee>()
+            {
+                new Employee(){Id = 1,Name = "Rohi",Email = "rc@gmail.com", Department = "CSE"},
+                new Employee(){Id = 2,Name = "Sumanth",Email = "sc@gmail.com", Department = "B.sc.Computers"}
+
+            };
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
